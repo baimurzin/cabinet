918a8a5a259f26831f9f6c9819a5b1f6044e2a3e
Alexey <taler2002@gmail.com>
2018-09-19 03:36:39 +0300

    database support feature - step 1

---
+++ src/main/java/org/itlabel/cabinet/repository/TaskRepository.java
@@ -0,0 +1,15 @@
+package org.itlabel.cabinet.repository;
+
+import org.itlabel.cabinet.model.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
+
+//    @Query("select task from tasks where task.id = :id")
+//    Task findById(@Param("id") Long id);
+}
+
